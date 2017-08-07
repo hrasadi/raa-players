8bca89221ce2c0d25b7265ddbe03da48c58b20db
Hamid <hr.asaadi@nexeven.com>
2017-08-07 15:36:22 -0400

    Add support for remaining intents

--- raa-alexa-skill/index.js
+++ raa-alexa-skill/index.js
@@ -17,8 +17,41 @@ const handlers = {
     'AMAZON.PauseIntent': function () {
         this.emit('AMAZON.StopIntent');
     },
+    'AMAZON.ResumeIntent': function () {
+        this.emit('LaunchRequest');
+    },
+    'AMAZON.LoopOffIntent': function () {
+        this.emit('Unsupported');
+    },
+    'AMAZON.LoopOnIntent': function () {
+        this.emit('Unsupported');
+    },
+    'AMAZON.NextIntent': function () {
+        this.emit('Unsupported');
+    },
+    'AMAZON.PreviousIntent': function () {
+        this.emit('Unsupported');
+    },
+    'AMAZON.RepeatIntent': function () {
+        this.emit('Unsupported');
+    },
+    'AMAZON.ShuffleOffIntent': function () {
+        this.emit('Unsupported');
+    },
+    'AMAZON.ShuffleOnIntent': function () {
+        this.emit('Unsupported');
+    },
+    'AMAZON.StartOverIntent': function () {
+        this.emit('Unsupported');
+    },
+    'MusicControlIntent': function () {
+        this.emit('Unsupported');
+    },    
     'SessionEndedRequest': function() {
         console.log("ended");
+    },
+    'Unsupported': function() {
+        this.emit(':tell', 'I cannot do that.');
     },
     'Unhandled': function() {
         this.emit(':tell', 'Sorry, I didn\'t get that.');
