4ffb4d0e868b7ffad34f3a55a7a0016e49253c54
Hamid <hr.asaadi@nexeven.com>
2017-08-10 16:04:06 -0400

    fix a bug in chrome-extension preventing connection to get closed when programs end

--- raa-chrome-extension/background.js
+++ raa-chrome-extension/background.js
@@ -1,31 +1,83 @@
 var radioStream = null;
 var radioPowerSwitch = false;
-
-var radioHasProgram = false;
-var radioProgramCyclesInSameStatus = 0;
 var currentProgram = '';
+var raa1Url = "http://raa.media:8000/raa1.ogg";
 
-const raa1Url = "http://raa.media:8000/raa1.ogg";
-const raa1StatusUrl = 'http://www.raa.media:8000/status-json.xsl';
+var raa1StatusUrl = 'http://www.raa.media:8000/status-json.xsl';
 
-var decidePlayback = function() {
-    if (radioHasProgram && radioPowerSwitch) {
-        // invalidate any previous players
-        if (radioStream) {
-            radioStream.src = "";
+var PlaybackManager = function(onRadioProgramBeginCallback, onRadioProgramEndCallback) {
+
+  this.radioHasProgram = false;
+  this.title = null;
+  this.cyclesInSameStatus = 0;
+  this.isPausedByUser = false;
+
+  this.loop = function(self, initialized) {
+    // default value in older browsers
+    initialized = typeof initialized  === 'undefined' ? true : initialized;
+    
+    self.readServerStatus(initialized);
+  }
+
+  this.readServerStatus = function(initialized) {
+    self = this;
+    $.get(raa1StatusUrl, function(data) {
+      self.title = data.icestats.source.title;
+      if (self.title && self.title != "" && self.title != "BLANK") {
+        /* We have program. Determine if the status has changed */
+        // If radio didn't have program before
+        if (!self.radioHasProgram) {
+          self.radioHasProgram = true;
+          self.cyclesInSameStatus = 1;
+        } else {
+          self.cyclesInSameStatus++;
         }
-        radioStream = new Audio("http://raa.media:8000/raa1.ogg");
-        radioStream.play();
-
-    // Do not shutdown the stream immediately when the radio program ends, give some time for playback the rest 
-    // In case the radio is turned off manually, we should go ahead and cut the stream
-    } else if ((radioPowerSwitch && radioProgramCyclesInSameStatus > 2) ||   
-                 !radioPowerSwitch) {
-        if (radioStream) {
-            radioStream.src = "";
-            radioStream = null;
+      } else {
+        // We do not have program. Determine if the status has changed
+        if (self.radioHasProgram) {
+          self.radioHasProgram = false;
+          self.cyclesInSameStatus = 1;
+        } else {
+          self.cyclesInSameStatus++;
         }
+      }
+      self.decideRadioStatus(initialized);
+    });
+  }
+
+  this.decideRadioStatus = function(initialized) {
+    if (this.radioHasProgram) {
+      onRadioProgramBeginCallback(this.title);
+
+      // Do not shutdown the stream immediately when the radio program ends,
+      // give some time for playback the rest
+      // If this is the first time we are here (uninitialized status) we
+      // should go ahead and set the status no matter what
+    } else if ((!this.radioHasProgram && this.cyclesInSameStatus > 2) || !initialized) {
+      onRadioProgramEndCallback();
     }
+  }
+}
+
+PlaybackManager.prototype.init = function() {
+  // start
+  self = this;
+  this.loop(this, false);
+  setInterval(function() {
+    self.loop(self);
+  }, 10000);
+}
+
+
+var flipRadioPowerStatus = function() {
+  if ($('#player')[0].paused) {
+    $('#player')[0].play();
+    playbackManager.isPausedByUser = false;
+  } else {
+    $('#player')[0].pause();
+    playbackManager.isPausedByUser = true;
+  }
+  $("#player-button").html(generatePlayerButton());
 }
 
 chrome.runtime.onMessage.addListener(
@@ -33,8 +85,7 @@ chrome.runtime.onMessage.addListener(
         if (!sender.tab) {
             if (request.hasOwnProperty("setRadioPowerSwitch")) {
                 radioPowerSwitch = request.setRadioPowerSwitch;
-                decidePlayback();
-            
+                // TODO            
             } else if (request.hasOwnProperty("getRadioPowerSwitch")) {
                 sendResponse({radioPowerSwitch: radioPowerSwitch});
             
@@ -45,28 +96,32 @@ chrome.runtime.onMessage.addListener(
     }
 );
 
-var readRadioTitle = function(){
-    var previousHasProgramStatus = radioHasProgram;
-    jQuery.get(raa1StatusUrl, function(data){
-        var title = data.icestats.source.title;
-        if (title && title != "" && title != "BLANK") {                 
-            radioHasProgram = true;
-            currentProgram = 'در حال پخش: ' + title;
-        } else {
-            radioHasProgram = false;
-            currentProgram = 'الان برنامه نداریم!';
+document.addEventListener('DOMContentLoaded', function() {
+    playbackManager = new PlaybackManager(function(title) {
+        currentProgram = 'در حال پخش: ' + title;
+
+        // don't waste resources! If player is already started continue
+        if (radioStream && !radioStream.paused) {
+            return;
         }
 
-        if (previousHasProgramStatus != radioHasProgram) {
-            radioProgramCyclesInSameStatus = 0;
-            decidePlayback();      
-        } else {
-            radioProgramCyclesInSameStatus++;
+        // invalidate any previous players
+        if (radioPowerSwitch) {
+            if (radioStream) {
+                radioStream.src = "";
+            }
+
+            radioStream = new Audio(raa1Url);
+            radioStream.play();
+        }
+    },function () {
+        currentProgram = 'الان برنامه نداریم!';
+
+        if (radioStream) {
+            radioStream.src = "";
+            radioStream = null;
         }
     });
-}
 
-document.addEventListener('DOMContentLoaded', function() {
-    readRadioTitle();
-    setInterval(readRadioTitle, 10000);
+    playbackManager.init();
 });
