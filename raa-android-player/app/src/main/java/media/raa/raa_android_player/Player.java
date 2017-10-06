df478720012cf625f4fad1c25bf1df6eba6dd5d4
Hamid <hr.asaadi@nexeven.com>
2017-10-06 16:09:58 -0400

    Service and notification parts are stable, now handle status and communication

--- raa-android-player/app/src/main/java/media/raa/raa_android_player/Player.java
+++ raa-android-player/app/src/main/java/media/raa/raa_android_player/Player.java
@@ -6,21 +6,25 @@ import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.design.widget.BottomNavigationView;
 import android.support.v4.app.FragmentManager;
+import android.support.v4.app.NotificationManagerCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.view.MenuItem;
 
 import java.util.Locale;
 
 import media.raa.raa_android_player.model.PlaybackService;
-import media.raa.raa_android_player.view.lineup.LineupFragment;
-import media.raa.raa_android_player.model.lineup.Program;
+import media.raa.raa_android_player.view.lineup.LineupContainerFragment;
 import media.raa.raa_android_player.view.settings.SettingsFragment;
 
 import static media.raa.raa_android_player.model.PlaybackService.ACTION_PLAY;
+import static media.raa.raa_android_player.model.PlaybackService.ACTION_STOP;
 
-public class Player extends AppCompatActivity implements LineupFragment.OnListFragmentInteractionListener, SettingsFragment.OnFragmentInteractionListener {
+public class Player extends AppCompatActivity implements SettingsFragment.OnFragmentInteractionListener {
 
     BottomNavigationView navigationView;
+
+    LineupContainerFragment lineupContainerFragment = LineupContainerFragment.newInstance();
+    SettingsFragment settingsFragment = SettingsFragment.newInstance();
 
     private BottomNavigationView.OnNavigationItemSelectedListener mOnNavigationItemSelectedListener
             = new BottomNavigationView.OnNavigationItemSelectedListener() {
@@ -59,28 +63,49 @@ public class Player extends AppCompatActivity implements LineupFragment.OnListFr
 
         // Show the lineup by default
         navigationView = (BottomNavigationView) findViewById(R.id.navigation);
-        navigationView.setSelectedItemId(R.id.navigation_lineup);
         navigationView.setOnNavigationItemSelectedListener(mOnNavigationItemSelectedListener);
-        displayLineupFragment();
-
-
-
     }
 
     @Override
     protected void onResume() {
-        startService(new Intent(getApplicationContext(), PlaybackService.class).setAction(ACTION_PLAY));
-
         super.onResume();
+
+        // Create a new instance each time application comes to foreground
+        lineupContainerFragment = LineupContainerFragment.newInstance();
+        settingsFragment = SettingsFragment.newInstance();
+
+        // Default tab is the lineup when we enter foreground
+        navigationView.setSelectedItemId(R.id.navigation_lineup);
+
+        // start playback
+        Intent intent = new Intent(getApplicationContext(), PlaybackService.class);
+        intent.setAction(ACTION_PLAY);
+        startService(intent);
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+
+        // If notifications are not allowed, playback will be stopped upon quit
+        if (!NotificationManagerCompat.from(getApplicationContext()).areNotificationsEnabled()) {
+            // If user does not allow notification, stop the service
+            Intent intent = new Intent(getApplicationContext(), PlaybackService.class);
+            intent.setAction(ACTION_STOP);
+            // Stop the playback
+            startService(intent);
+            // Kill service
+            stopService(intent);
+        }
     }
 
     private void displayLineupFragment() {
         //noinspection ConstantConditions
         this.getSupportActionBar().setTitle(getString(R.string.title_lineup));
-        LineupFragment fragment = LineupFragment.newInstance();
+
         FragmentManager fragmentManager = getSupportFragmentManager();
         fragmentManager.beginTransaction()
-                .replace(R.id.application_frame, fragment).commit();
+                .replace(R.id.application_frame, lineupContainerFragment).commit();
     }
 
     private void openPodcast() {
@@ -92,17 +117,13 @@ public class Player extends AppCompatActivity implements LineupFragment.OnListFr
     private void displaySettingsFragment() {
         //noinspection ConstantConditions
         this.getSupportActionBar().setTitle(getString(R.string.title_settings));
-        SettingsFragment fragment = SettingsFragment.newInstance();
         FragmentManager fragmentManager = getSupportFragmentManager();
         fragmentManager.beginTransaction()
-                .replace(R.id.application_frame, fragment).commit();
+                .replace(R.id.application_frame, settingsFragment).commit();
     }
 
+    @Override
     public void onFragmentInteraction(Uri uri) {
-
-    }
-
-    public void onListFragmentInteraction(Program item) {
 
     }
 }
